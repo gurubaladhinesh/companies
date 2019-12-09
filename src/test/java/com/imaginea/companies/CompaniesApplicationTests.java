7b9015a388bbf8a5fb5981999f3ff4a520a1e672
gurubalad <gurubala.dhinesh@imaginea.com>
2019-12-09 11:36:29 +0530

    initial commit

---
+++ src/test/java/com/imaginea/companies/CompaniesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imaginea.companies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompaniesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
